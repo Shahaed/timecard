6bb77548adc9516f1e1ea4e53a1b85d34b393130
RafaelBroseghini <rafaellopesbroseghini@gmail.com>
2019-11-08 00:35:23 -0600

    fourth

--- server/app.py
+++ server/app.py
@@ -24,5 +24,16 @@ def hello():
     return json.dumps(rows)
 
 
+@app.route("/task", methods=["POST"])
+def handle_add_task():
+    start_time = request.form['start_time']
+    end_time = request.form['end_time']
+    deliverable = request.form['deliverable']
+    date = request.form['date']
+
+    # Select from table here:
+    pass
+
+
 if __name__ == '__main__':
     app.run()
