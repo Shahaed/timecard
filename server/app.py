bbdd1f5ec4497b4192e62fc2a955e7d9a2a173fb
RafaelBroseghini <rafaellopesbroseghini@gmail.com>
2019-11-08 00:29:18 -0600

    third commit

--- server/app.py
+++ server/app.py
@@ -1,18 +1,27 @@
 import sqlite3
+import json
 from flask import Flask
 app = Flask(__name__)
 
 conn = sqlite3.connect('timecard.db')
 
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 @app.route('/')
 def hello():
+    conn.row_factory = dict_factory
     c = conn.cursor()
     c.execute('''SELECT * from account''')
 
     rows = c.fetchall()
     conn.close()
-    return rows
+    return json.dumps(rows)
 
 
 if __name__ == '__main__':
